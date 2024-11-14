3b88db8edc98b6e8fb644a3964ef49975173a4e0
hihihi99999 <44358364+hihihi99999@users.noreply.github.com>
2024-11-14 21:57:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 import streamlit as st 
 
-dify_api_key = st.secrets.dify_api_key.dify_api_key
+dify_api_key = st.secrets.difyapikey.dify_api_key
 
 url = "https://api.dify.ai/v1/chat-messages"
 
